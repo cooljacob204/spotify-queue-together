7c591b4d3d877a071457b7b35867b6fc289777cc
Jacob Feldman <me@jacobfeldman.dev>
2022-04-02 18:00:08 -0400

    Adds id to room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,13 @@
 class Room
+  attr_reader :id
+
   class << self
     def create
-      Array.new(5){[*"A".."Z", *"0".."9"].sample}.join
+      new(Array.new(5) { [*'A'..'Z', *'0'..'9'].sample }.join)
     end
+  end
+
+  def initialize(id)
+    @id = id
   end
 end
