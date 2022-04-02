726526ce584f5348948ea7efd93fb6d489f40e72
Jacob Feldman <me@jacobfeldman.dev>
2022-04-02 17:41:41 -0400

    Autoloads adapter path

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module SpotifyQueueTogether
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.autoload_paths += %W(#{Rails.root}/app/adapters)
 
     # Configuration for the application, engines, and railties goes here.
     #
