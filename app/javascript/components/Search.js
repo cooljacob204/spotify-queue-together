c1785a63f3ee1889174f32fb5a458af7b63d44fe
Carlos Quinones <cquinones100@gmail.com>
2022-04-03 13:56:50 -0400

    add preview to search

--- app/javascript/components/Search.js
+++ app/javascript/components/Search.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react"
+import React, { useEffect, useState, useRef } from "react"
 import PropTypes from "prop-types"
+import { Binding } from "@babel/traverse";
 const Search = ({ token } ) => {
   const [value, setValue] = useState('');
   const [tracks, setTracks] = useState([]);
+  const [currentlyPlayingPreview, setCurrentlyPlayingPreview] = useState(null);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -31,6 +33,30 @@ const Search = ({ token } ) => {
     return <>You must have a token</>
   }
 
+  const handleCurrentlyPlayingChange = (preview_url) => {
+    if (currentlyPlayingPreview === preview_url) {
+      previousAudio.current.src = '';
+
+      setCurrentlyPlayingPreview(null);
+    } else {
+      setCurrentlyPlayingPreview(preview_url);
+    }
+  }
+
+  const previousAudio = useRef();
+
+  useEffect(() => {
+    if (previousAudio.current) {
+      previousAudio.current.src = '';
+    }
+
+    if (currentlyPlayingPreview) {
+      previousAudio.current = new Audio(currentlyPlayingPreview);
+
+      previousAudio.current.play();
+    }
+  }, [currentlyPlayingPreview])
+
   return (
     <>
       <form onSubmit={handleFormSubmit}>
@@ -52,9 +78,17 @@ const Search = ({ token } ) => {
             </tr>
           </thead>
           <tbody>
-            {tracks.map(({ name, artists, album: { images } }) => (
-              <tr>
-                <th><img src={images.find(({ height }) => height === 64).url} /></th>
+            {tracks.map(({ name, artists, preview_url, album: { images } }) => (
+              <tr key={preview_url}>
+                <th className='album-art'>
+                  <img src={images.find(({ height }) => height === 64).url} />
+                  <button
+                    className="btn"
+                    onClick={() => { handleCurrentlyPlayingChange(preview_url) }}
+                  >
+                    {currentlyPlayingPreview === preview_url ? '||' : '▶'}
+                  </button>
+                </th>
                 <th>{name}</th>
                 <th>{artists.reduce((acc, { name }) => acc += (name + ' '), '')}</th>
               </tr>
